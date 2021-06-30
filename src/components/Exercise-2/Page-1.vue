e9ee66cab60fead8f79adb52e272e4eca187dc1c
ottonomous <ottonomous@github.com>
2021-06-29 17:43:10 -0700

    add before routeLeave + dialog

--- src/components/Exercise-2/Page-1.vue
+++ src/components/Exercise-2/Page-1.vue
@@ -3,7 +3,8 @@ import { mapGetters, mapMutations } from 'vuex'
 export default {
   data() {
     return {
-      input: null
+      input: null,
+      showNavWarningDialog: false
     }
   },
   computed: {
@@ -11,6 +12,12 @@ export default {
     inputMatches() {
       return this.savedInput == this.input
     }
+  },
+  beforeRouteLeave (to, from , next) {
+    if(!this.inputMatches) {
+      this.showNavWarningDialog = true
+    }
+    else next()
   },
   mounted() {
     this.input = this.savedInput
@@ -29,6 +36,21 @@ export default {
     <h3>Page 1</h3>
     <textarea v-model="input" placeholder="Type something here..." />
     <button class="ripple" :disabled="inputMatches" @click="save">Save</button>
+    <div role="dialog" v-if="showNavWarningDialog" class="navWarningDialog">
+      <div class="dialog-container">
+        <div class="dialog-header">
+          <i class="fa fa-warning"></i> Unsaved Changes
+        </div>
+
+        <div class="dialog-body">
+          Your changes will be lost if you do not save them. Would you like to save your changes before leaving this page? 
+        </div>
+
+        <div class="dialog-footer">
+          buttons go here
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -37,6 +59,61 @@ export default {
   display: flex;
   flex-direction: column;
   text-align: center;
+  position: relative;
+}
+
+.navWarningDialog{
+  background-color:rgba(47, 41, 56, 0.493);
+  position: absolute;
+  margin-top: 80px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  right: 0;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  transition: opacity 0.3s ease;
+
+  .dialog-container{
+    z-index: 10000;
+    background-color: white;
+    border: 5px solid #f3d138;
+    margin: auto;
+    left: 0;
+    right: 0;
+    height: 55%;
+    width: 70%;
+    box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.26);
+
+    .dialog-header{
+      padding: 15px 0px 5px 0px;
+      margin-bottom: 10px;
+      font-size: 1.2rem;
+      font-weight: bold;
+      color: #f3d138;
+      text-align: center;
+      border-bottom: 2px solid #f3d138;
+    } 
+
+    .dialog-body{
+      padding: 10px 40px 20px 40px;
+      margin-bottom: 10px;
+      font-size: 0.95em;
+      color: black;
+      text-align: left;
+    }
+
+    .dialog-footer{
+      padding: 5px 35px 5px 35px;
+      display: flex;
+      direction: row;
+      justify-content: space-between;
+    }
+
+  }
 }
 
 button {
