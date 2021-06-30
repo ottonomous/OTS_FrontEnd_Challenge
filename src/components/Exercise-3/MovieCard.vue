7a7a99d174e4ab7678a6964dd7d3768e72b195cc
ottonomous <ottonomous@github.com>
2021-06-29 18:48:13 -0700

    add poster + ranking to movieCard

--- src/components/Exercise-3/MovieCard.vue
+++ src/components/Exercise-3/MovieCard.vue
@@ -1,13 +1,48 @@
 <template>
-  
+  <div class="movie-card">
+    <div class="movie-ranking">
+      {{ranking + "."}}
+    </div>
+    <div> 
+      <img :src="movie.poster"/>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
 
+export default {
+  name: "MovieCard",
+  props: ['movie', 'ranking']
 }
+
 </script>
 
-<style>
+<style lang="scss" scoped>
 
+img {
+  width: 12em;
+  
+}
+
+.movie-ranking{
+  font-size: 2em;
+  font-weight: bold;
+  padding: 10px;
+  margin: 20px;
+}
+
+.movie-card {
+   border-radius: 25px;
+    padding: 20px;
+    margin: 50px;
+    background-color: rgba(133, 167, 168, 0.158);
+    display: flex;
+    flex-direction: row;
+    height: 300px;
+    justify-self: center;
+    margin: 20px;
+    text-align: left;
+    width: 90%;
+  }
 </style>
